e68a1c80d76c53dbafe76120015d50e25be63ca8
William Rhodes <williamrhodes@Williams-MacBook-Pro.local>
2020-12-18 00:48:32 -0600

    update

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -22,7 +22,7 @@ export const authenticate = (userId, token, expiryTime) => {
 export const signup = (email, password) => {
   return async dispatch => {
     const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAwgME943HeTkyX9QJak260PNbYZfQI58Q',
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBZztLmhmjlJVojFklzQtSXqhWHuh_8Lfg',
       {
         method: 'POST',
         headers: {
@@ -65,7 +65,7 @@ export const signup = (email, password) => {
 export const login = (email, password) => {
   return async dispatch => {
     const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAwgME943HeTkyX9QJak260PNbYZfQI58Q',
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBZztLmhmjlJVojFklzQtSXqhWHuh_8Lfg',
       {
         method: 'POST',
         headers: {
